28e5420e6de372a8f3894b19b90c929d8ec9ad7c
KunHee Lim <rg4093@naver.com>
2023-11-06 21:59:43 +0900

    feat: ui compontnets 추가

---
+++ src/constant/index.ts
@@ -0,0 +1,43 @@
+export const menus = [
+  {
+    name: "KunHee Blog",
+    href: "/",
+    icon: "",
+  },
+  {
+    name: "posts",
+    href: "/posts",
+  },
+  {
+    name: "about",
+    href: "/about",
+  },
+  {
+    name: "guestbook",
+    href: "/guestbook",
+  },
+  {
+    name: "playground",
+    href: "/playground",
+  },
+  {
+    name: "Search",
+
+    icon: "",
+  },
+];
+
+export const navMenus = [
+  {
+    name: "github",
+    href: "https://github.com/rg5668",
+  },
+  {
+    name: "linkedIn",
+    href: "https://www.linkedin.com/in/kh5668/",
+  },
+  {
+    name: "resume",
+    href: "",
+  },
+];
