cf8a5b7c7731e991d22c44016ff3655cf3842e57
KunHee Lim <rg4093@naver.com>
2023-11-08 20:11:14 +0900

    rename: folder 변경

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,8 +2,8 @@ import { ChildrenProps } from "@/app/layout";
 
 export default function Layout({ children }: ChildrenProps) {
   return (
-    <section className="relative">
-      <div className="max-w-3xl mx-auto my-0 p-4">{children}</div>
-    </section>
+    <div className="relative">
+      <div className="max-w-3xl mx-auto my-2 pt-4">{children}</div>
+    </div>
   );
 }
