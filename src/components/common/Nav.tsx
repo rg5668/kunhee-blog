51b59f48193f4d15b3fdf55e5f9d316c6fc434b3
KunHee Lim <rg4093@naver.com>
2023-11-14 17:49:45 +0900

    feat: Header Search Bar Animation 적용

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <nav className="h-10 sticky top-0 z-10">
+    <nav className="h-10 sticky top-0 z-[1]">
       <div className="text-center bg-rgba-white backdrop-blur-sm border-b border-gray-200 max-w-3xl flex h-10 mx-auto my-0 items-center">
         <h1 className="flex-grow">최신글 올라오기</h1>
         {/* <ul className="flex gap-4">
