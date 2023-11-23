f535bc9a8f0523fea0adc2654d713d5f3a81cb42
KunHee Lim <rg4093@naver.com>
2023-11-23 17:59:50 +0900

    feat: main page image 변경 close #22 #23

--- src/components/Posts/Post.tsx
+++ src/components/Posts/Post.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
-// 임시 이미지
-import dog from '/public/dog.jpg';
 
-export default function Post() {
+interface Post {
+  imageSrc?: string;
+}
+
+export default function Post({ imageSrc }: Post) {
   // w-[230px] -md:max-w-[200px] -sm:max-w-full
   return (
     <Link
@@ -11,7 +13,15 @@ export default function Post() {
       className='items-center border rounded-2xl scale-100 hover:scale-95 transition-all w-[calc(33%-6px)] -sm:w-[calc(50%-6px)] -xs:w-[calc(50%-8px)] -xxs:w-full'
     >
       <div className='items-center flex-wrap flex'>
-        <Image src={dog} alt={''} width={0} height={0} unoptimized={true} className='rounded-t-2xl' />
+        <Image
+          src={imageSrc ? imageSrc : '/assets/no_image.png'}
+          alt={''}
+          width={200}
+          height={200}
+          unoptimized={true}
+          className='rounded-t-2xl h-auto w-full'
+        />
+
         <span className='text-base -md:text-sm font-bold p-3 overflow-hidden -md:h-[54px] line-clamp-2'>
           클로저에 대해서 클로저에 대해서 클로저에 대해서
         </span>
--- src/components/Posts/PostList.tsx
+++ src/components/Posts/PostList.tsx
@@ -5,26 +5,28 @@ import { FaPlus, FaArrowRight } from 'react-icons/fa';
 export default function PostList() {
   return (
     <>
-      <div className='text-xl -md:text-lg -sm:text-base font-bold pb-4 pt-8'>
-        <h1>Study</h1>
+      <div className='flex items-center justify-between pb-4 pt-4'>
+        <h1 className='text-xl -md:text-lg -sm:text-base font-bold'>Study</h1>
+
+        <Link
+          href={`/`}
+          className='font-bold hover-underline-animation flex items-center gap-2 after:dark:bg-white after:bg-black opacity-[0.7] hover:opacity-[1] -md:text-sm -sm:text-[13px] -xs:text-xs'
+        >
+          Title 페이지 이동 <FaArrowRight />
+        </Link>
       </div>
       <div className='flex flex-wrap gap-3 -xs:justify-center'>
-        {new Array(4).fill(0).map((v, i) => (
-          <Post key={i} />
+        {new Array(8).fill(0).map((v, i) => (
+          <Post key={i} imageSrc={''} />
         ))}
       </div>
-      <div className='pt-5 flex gap-5 justify-center -md:text-sm -sm:text-xs'>
+      <div className='pt-5 pb-8 -md:text-sm -sm:text-xs'>
         <Link
           href={'/'}
-          className='dark:bg-white dark:text-black text-white bg-black w-auto rounded-2xl px-5 py-2 -xs:px-3 -xs:py-2 box-border outline-none justify-center items-center font-bold flex'
+          className='dark:bg-white hover:opacity-100 opacity-80 dark:text-black text-white bg-black w-auto rounded-2xl px-5 py-2 -xs:px-3 -xs:py-2 box-border outline-none justify-center items-center font-bold flex'
         >
-          게시글 더보기 <FaPlus />
-        </Link>
-        <Link
-          href={'/'}
-          className='dark:bg-white dark:text-black text-white bg-black w-auto rounded-2xl px-5 py-2 -xs:px-3 -xs:py-2 box-border outline-none justify-center items-center font-bold flex'
-        >
-          title 페이지 이동 <FaArrowRight />
+          게시글 더보기 &nbsp;
+          <FaPlus />
         </Link>
       </div>
     </>
