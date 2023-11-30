b521af92b362994e1fb062f02ffc0baa05acb70c
KunHee Lim <rg4093@naver.com>
2023-11-30 21:16:54 +0900

    feat: mock data 추가

---
+++ src/constant/about.ts
@@ -0,0 +1,52 @@
+export const about = {
+  careers: [
+    {
+      name: '뷰메진',
+      startDate: '2022.09',
+      endDate: '2023.11',
+      team: 'Front-end',
+      description: [
+        'VODA 서비스 플랫폼 운영 및 개발',
+        'Components Storybook 운영 및 개발',
+        '건설사(H사, P사) B2B 서비스 개발 (VODA 서비스 도입)',
+      ],
+      portfolioLink: 'https://strong-sunshine-6c9.notion.site/VODA-c7356aa3fcc2414284dfff08f59cb793',
+      webSiteLink: 'https://www.voda.vision/landing',
+    },
+    {
+      name: '와이즈커머스',
+      startDate: '2021.09',
+      endDate: '2022.02',
+      team: 'Front-end',
+      description: ['Gentlemonster 반응형 웹 운영 및 개발'],
+      webSiteLink: 'https://www.gentlemonster.com/kr/',
+    },
+  ],
+  toyProjects: [
+    {
+      name: '3D-Modeling',
+      startDate: '2023.02',
+      endDate: '2023.02',
+      team: '개인',
+      portfolioLink: 'https://strong-sunshine-6c9.notion.site/3D-Model-Control-e2bf8e47349d4fd89d24871c848e99d8',
+      webSiteLink: 'https://3d-modeling-rg5668.vercel.app/',
+      github: 'https://github.com/rg5668/3d-modeling',
+    },
+  ],
+  other: [
+    {
+      name: '신구대학교',
+      startDate: '2015.03',
+      endDate: '2021.02',
+      description: ['IT 소프트웨어과에서 공부하며 자료구조, 웹프로그래밍등의 과목을 이수하였습니다.'],
+    },
+    {
+      name: '케이아이씨캠퍼스',
+      startDate: '2020.12',
+      endDate: '2021.06',
+      description: [
+        'Spring 기반 웹 개발로 국비 교육 과정을 이수하였고, 30명 수강생 중 3등으로 교육 과정을 수료하여 우수상을 수상하였습니다.',
+      ],
+    },
+  ],
+};
---
+++ src/constant/playground.ts
@@ -0,0 +1,26 @@
+export const playgrounds = [
+  {
+    title: '3D Model Control',
+    description: 'Model GUI Controller 개발',
+    link: 'https://3d-modeling-rg5668.vercel.app',
+    imageSrc: '/assets/3d-seo.jpg',
+  },
+  {
+    title: '3D Model Control',
+    description: 'Model GUI Controller 개발',
+    link: 'https://3d-modeling-rg5668.vercel.app',
+    imageSrc: '/assets/3d-seo.jpg',
+  },
+  {
+    title: '3D Model Control',
+    description: 'Model GUI Controller 개발',
+    link: 'https://3d-modeling-rg5668.vercel.app',
+    imageSrc: '/assets/3d-seo.jpg',
+  },
+  {
+    title: '3D Model Control',
+    description: 'Model GUI Controller 개발',
+    link: 'https://3d-modeling-rg5668.vercel.app',
+    imageSrc: '/assets/3d-seo.jpg',
+  },
+];
