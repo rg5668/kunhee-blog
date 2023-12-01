02c5457c61e6932b00798d97e08be3db9c018dea
KunHee Lim <rg4093@naver.com>
2023-12-01 13:25:29 +0900

    feat: about content 추가

--- src/constant/about.ts
+++ src/constant/about.ts
@@ -4,7 +4,7 @@ export const about = {
       name: '뷰메진',
       startDate: '2022.09',
       endDate: '2023.11',
-      team: 'Front-end',
+      team: '프론트엔드 개발팀',
       description: [
         'VODA 서비스 플랫폼 운영 및 개발',
         'Components Storybook 운영 및 개발',
@@ -12,41 +12,53 @@ export const about = {
       ],
       portfolioLink: 'https://strong-sunshine-6c9.notion.site/VODA-c7356aa3fcc2414284dfff08f59cb793',
       webSiteLink: 'https://www.voda.vision/landing',
+      skills: 'React.js · Next.js · Typescript · Recoil · Emotion · React-Query · Tailwind',
     },
     {
       name: '와이즈커머스',
       startDate: '2021.09',
       endDate: '2022.02',
-      team: 'Front-end',
+      team: '프론트엔드 개발팀',
       description: ['Gentlemonster 반응형 웹 운영 및 개발'],
       webSiteLink: 'https://www.gentlemonster.com/kr/',
+      skills: 'JQuery',
     },
   ],
   toyProjects: [
+    {
+      name: 'Kunhee Blog',
+      startDate: '2023.11',
+      endDate: '2023.12',
+      team: '개인',
+      description: ['(현재 페이지)', 'Next를 이용하여 정적 블로그 개발 (기술 블로그 운영 목적)'],
+      // 추후 데이터 바꿔야함.
+      portfolioLink: 'https://strong-sunshine-6c9.notion.site/3D-Model-Control-e2bf8e47349d4fd89d24871c848e99d8',
+      webSiteLink: 'https://3d-modeling-rg5668.vercel.app/',
+      githubLink: 'https://github.com/rg5668/3d-modeling',
+      skills: 'React.js · Next.js · Typescript · Tailwind',
+    },
     {
       name: '3D-Modeling',
       startDate: '2023.02',
       endDate: '2023.02',
       team: '개인',
+      description: [
+        'React와 Threejs를 사용해 Model(.gltf) GUI Controller 개발',
+        'Vercel을 통해 배포',
+        '@react-three/drei, @react-three/fiber, @react-dat-gui 사용',
+      ],
       portfolioLink: 'https://strong-sunshine-6c9.notion.site/3D-Model-Control-e2bf8e47349d4fd89d24871c848e99d8',
       webSiteLink: 'https://3d-modeling-rg5668.vercel.app/',
-      github: 'https://github.com/rg5668/3d-modeling',
+      githubLink: 'https://github.com/rg5668/3d-modeling',
+      skills: 'React.js · Typescript',
     },
   ],
-  other: [
+  others: [
     {
       name: '신구대학교',
       startDate: '2015.03',
       endDate: '2021.02',
       description: ['IT 소프트웨어과에서 공부하며 자료구조, 웹프로그래밍등의 과목을 이수하였습니다.'],
-    },
-    {
-      name: '케이아이씨캠퍼스',
-      startDate: '2020.12',
-      endDate: '2021.06',
-      description: [
-        'Spring 기반 웹 개발로 국비 교육 과정을 이수하였고, 30명 수강생 중 3등으로 교육 과정을 수료하여 우수상을 수상하였습니다.',
-      ],
     },
   ],
 };
