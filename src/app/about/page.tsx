54c08e44af609445ae36df1a1d25d37edafeee59
KunHee Lim <rg4093@naver.com>
2023-12-01 14:01:10 +0900

    feat: about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,12 @@
+import AboutList from '@/components/abouts/AboutList';
+import MainBanner from '@/components/common/MainBanner';
+
 export default function About() {
-  return <>About</>;
+  return (
+    <div>
+      <h1 className='text-lg font-bold pb-4 mb-7 text-center border-b-2 underline underline-offset-4'>About</h1>
+      <MainBanner />
+      <AboutList />
+    </div>
+  );
 }
