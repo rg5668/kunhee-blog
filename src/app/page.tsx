c5eaede24f33c0b5741e4fea4da4dd793cd0bb5a
KunHee Lim <rg4093@naver.com>
2023-12-02 21:35:08 +0900

    feat: post page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
-import PostList from '@/components/posts/PostList';
 import MainBanner from '@/components/common/MainBanner';
+import PostList from '@/components/posts/PostList';
+import { getAllPosts } from '@/service/posts';
+import { PostProps } from '@/types/post';
 
-export default function Home() {
+export default async function Home() {
+  const { posts, category } = (await getAllPosts([
+    'slug',
+    'title',
+    'date',
+    'category',
+    'description',
+    'coverImage',
+  ])) as unknown as PostProps;
   return (
-    <div>
+    <>
       <MainBanner />
-      <PostList />
-      <PostList />
-      <PostList />
-      <PostList />
-    </div>
+      <PostList posts={posts} category={category} />
+    </>
   );
 }
