741ebdc2adbe54f325ce11df05d5ba9a529eac25
KunHee Lim <rg4093@naver.com>
2023-12-02 21:34:04 +0900

    feat: post service

---
+++ src/service/posts.ts
@@ -0,0 +1,72 @@
+import path from 'path';
+import { promises as fs } from 'fs';
+import matter from 'gray-matter';
+import { cache } from 'react';
+
+const postsDirectory = path.join(process.cwd(), '__posts');
+
+export async function getPostSlugs() {
+  const subdirectories = await fs.readdir(postsDirectory);
+
+  const allFiles = await Promise.all(
+    subdirectories.map(async (subdirectory) => {
+      const files = await fs.readdir(path.join(postsDirectory, subdirectory));
+      return files.map((file) => path.join(subdirectory, file));
+    }),
+  );
+
+  const flattenedFiles = allFiles.flat();
+
+  return {
+    subdirectories,
+    files: flattenedFiles,
+  };
+}
+
+export async function getPostBySlug(slug: string, fields: string[] = []) {
+  const realSlug = slug.replace(/\.md$/, '');
+  const [subdirectory, fileName] = realSlug.split('/');
+  const fullPath = path.join(postsDirectory, subdirectory, `${fileName}.md`);
+
+  try {
+    await fs.access(fullPath);
+
+    const fileContents = await fs.readFile(fullPath, 'utf8');
+    const { data, content } = matter(fileContents);
+
+    const items: Record<string, string> = {};
+
+    fields.forEach((field) => {
+      if (field === 'slug') {
+        items[field] = realSlug;
+      }
+      if (field === 'content') {
+        items[field] = content;
+      }
+      if (typeof data[field] !== 'undefined') {
+        items[field] = data[field];
+      }
+    });
+
+    return items;
+  } catch (error) {
+    return {
+      slug: '404',
+    };
+  }
+}
+export const getAllPosts = cache(async (fields: string[] = []) => {
+  const { subdirectories, files } = await getPostSlugs();
+
+  const posts = await Promise.all(
+    files.map(async (filePath: string) => {
+      const post = await getPostBySlug(filePath, fields);
+      return post;
+    }),
+  );
+
+  return {
+    posts: posts.sort((post1, post2) => (post1.date > post2.date ? -1 : 1)),
+    category: subdirectories,
+  };
+});
