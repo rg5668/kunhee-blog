bccfc7457eb6ffd39280fcb0e7e8102764645a07
KunHee Lim <rg4093@naver.com>
2023-12-03 21:02:10 +0900

    docs: constant data 수정

--- src/constant/about.ts
+++ src/constant/about.ts
@@ -6,9 +6,8 @@ export const about = {
       endDate: '2023.11',
       team: '프론트엔드 개발팀',
       description: [
-        'VODA 서비스 플랫폼 운영 및 개발',
+        'Next를 활용한 자사/고객 서비스 플랫폼 운영 및 개발 (서비스명 : VODA)',
         'Components Storybook 운영 및 개발',
-        '건설사(H사, P사) B2B 서비스 개발 (VODA 서비스 도입)',
       ],
       webSiteLink: 'https://www.voda.vision/landing',
       skills: ['React.js', 'Next.js', 'Typescript', 'Recoil', 'Emotion', 'React-Query', 'Tailwind'],
@@ -29,12 +28,11 @@ export const about = {
       startDate: '2023.11',
       endDate: '2023.12',
       team: '개인',
-      description: ['(현재 페이지)', 'Next를 이용하여 정적 블로그 개발 (기술 블로그 운영 목적)'],
-      // todo 추후 데이터 바꿔야함.
-      portfolioLink: 'https://strong-sunshine-6c9.notion.site/3D-Model-Control-e2bf8e47349d4fd89d24871c848e99d8',
-      webSiteLink: 'https://3d-modeling-rg5668.vercel.app/',
-      githubLink: 'https://github.com/rg5668/3d-modeling',
-      skills: ['React.js', 'Next.js', 'Typescript', 'Tailwind'],
+      description: ['(현재 페이지)', 'Next에 SSG를 활용한 정적 블로그 개발 (기술 블로그 운영 목적)'],
+      portfolioLink: 'https://strong-sunshine-6c9.notion.site/Kunhee-Blog-87d2b935d38a456da9b7c41cbaa5d188?pvs=4',
+      webSiteLink: 'https://kunhee-blog.vercel.app/',
+      githubLink: 'https://github.com/rg5668/kunhee-blog',
+      skills: ['Next.js', 'Typescript', 'Tailwind'],
     },
     {
       name: '3D-Modeling',
@@ -58,6 +56,14 @@ export const about = {
       startDate: '2015.03',
       endDate: '2021.02',
       description: ['IT 소프트웨어과에서 공부하며 자료구조, 웹프로그래밍등의 과목을 이수하였습니다.'],
+    },
+    {
+      name: '케이아이씨캠퍼스',
+      startDate: '2020.12',
+      endDate: '2021.06',
+      description: [
+        'Spring 기반 웹 개발로 국비 교육 과정을 이수하였고, 30명 수강생 중 3등으로 교육 과정을 수료하여 우수상을 수상하였습니다.',
+      ],
     },
   ],
 };
--- src/constant/playground.ts
+++ src/constant/playground.ts
@@ -1,16 +1,16 @@
 export const playgrounds = [
   {
     title: 'Kunhee Blog',
-    description: '2023.11 ~ 진행, 기술 블로그 직접 개발 및 운영',
-    targetLink: 'https://3d-modeling-rg5668.vercel.app',
-    coverImage: '/assets/3d-seo.jpg',
+    description: '2023.11 ~ 진행, Next에 SSG를 활용한 정적 블로그 개발',
+    targetLink: 'https://kunhee-blog-2kib.vercel.app',
+    coverImage: '/assets/kunhee-blog.png',
     team: '개인',
   },
   {
     title: '3D Model Control',
     description: '2023.02 ~ 2023.02, Model GUI Controller 개발',
     targetLink: 'https://3d-modeling-rg5668.vercel.app',
-    coverImage: '/assets/3d-seo.jpg',
+    coverImage: '/assets/3d-controller.jpg',
     team: '개인',
   },
 ];
