79c58402debd71fee7166f27d20613778c88bb41
KunHee Lim <rg4093@naver.com>
2023-12-04 23:03:18 +0900

    fix: posts slugs posts/ 추가

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,7 +5,7 @@ import { MetadataRoute } from 'next';
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const blogPosts = await getAllPosts(['slug']);
 
-  const slugs = blogPosts.posts.map((v) => v.slug);
+  const slugs = blogPosts.posts.map((v) => 'posts/' + v.slug);
   const urls = [...slugs, ...meta.url_all.map((v) => (v === '/' ? v.replace(v, '') : v))].map((url) => {
     return {
       url: `${meta.url}/${url}`,
