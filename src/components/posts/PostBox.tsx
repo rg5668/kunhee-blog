c9552c848af3677d6d8116c7c544c7367176a56e
KunHee Lim <rg4093@naver.com>
2023-12-04 16:42:01 +0900

    feat: Post 목차 Components, ProgressBar Components

--- src/components/posts/PostBox.tsx
+++ src/components/posts/PostBox.tsx
@@ -5,34 +5,53 @@ import HoverLink from '../ui/HoverLink';
 import { FaLongArrowAltLeft, FaLongArrowAltRight } from 'react-icons/fa';
 import { useRouter } from 'next/navigation';
 import { PostBoxProps } from '@/types/post';
+import Image from 'next/image';
 
-export default function PostBox({ post }: PostBoxProps) {
+export default function PostBox({ post, prev }: PostBoxProps) {
   const router = useRouter();
   return (
-    post.currentIndex && (
-      <div className='flex items-center justify-between pt-5 border-t'>
-        <HoverLink
-          onClick={() => router.push(`/posts/${post?.prev?.slug}`)}
-          animation={post.currentIndex > 1}
-          disabled={post.currentIndex <= 1}
-          className='flex items-center gap-2'
-        >
-          <FaLongArrowAltLeft className='w-5 h-5' />
-          이전글
-        </HoverLink>
-        <div className='flex gap-2'>
-          <span>{post.currentIndex}</span>/<span>{post.totalLength}</span>
+    <HoverLink
+      className={`flex w-full hover:outline hover:outline-1 hover:outline-gray-300 hover:rounded-xl hover:bg-gray-200 hover:dark:bg-slate-800`}
+      onClick={() => router.push(`/posts/${post.slug}`)}
+    >
+      <div
+        className={`flex flex-wrap ${
+          prev && 'flex-row-reverse'
+        } w-full justify-between -sm:justify-center items-center py-3 px-8 border rounded-lg`}
+      >
+        <div className={`flex flex-wrap -sm:justify-center items-center gap-4 ${prev && 'flex-row-reverse'}`}>
+          <Image
+            src={post.coverImage ? post.coverImage : '/assets/no_image.png'}
+            width={100}
+            height={100}
+            alt=''
+            priority
+            className='w-[100px] h-[100px] object-cover'
+          />
+          <div className={`flex -sm:items-center flex-col ${prev ? 'items-end' : 'items-start'} w-auto gap-[2px]`}>
+            {post.category && (
+              <span className='inline-block px-3 py-1 mb-2 text-xs border border-gray-300 rounded-xl'>
+                {post.category}
+              </span>
+            )}
+            <span className='font-bold'>{post.title}</span>
+            <span className='text-sm'>{post.description}</span>
+            <span className='text-sm'>{post.date}</span>
+          </div>
         </div>
-        <HoverLink
-          onClick={() => router.push(`/posts/${post?.next?.slug}`)}
-          animation={post.currentIndex !== post.totalLength}
-          disabled={post.currentIndex === post.totalLength}
-          className='flex items-center gap-2'
-        >
-          다음글
-          <FaLongArrowAltRight className='w-5 h-5' />
-        </HoverLink>
+        <div className='flex items-center gap-3'>
+          {prev ? (
+            <>
+              <FaLongArrowAltLeft className='w-5 h-5' />
+              이전글
+            </>
+          ) : (
+            <>
+              다음글 <FaLongArrowAltRight className='w-5 h-5' />
+            </>
+          )}
+        </div>
       </div>
-    )
+    </HoverLink>
   );
 }
---
+++ src/components/posts/ProgressBar.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { throttle } from '@/utill/throttle';
+import { useEffect, useState } from 'react';
+
+export default function ProgressBar() {
+  const [width, setWidth] = useState(0);
+
+  const handleScroll = throttle((): void => {
+    const scrollY = window.scrollY;
+    const { scrollHeight, clientHeight } = document.documentElement;
+    if (scrollY === 0) return setWidth(0);
+    const windowHeight = scrollHeight - clientHeight;
+    const currentPercent = scrollY / windowHeight;
+    setWidth(currentPercent * 100);
+  }, 200);
+
+  useEffect(() => {
+    window.document.documentElement.scrollTo(0, 0);
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className='fixed left-0 top-[48px] z-20 w-full h-4 bg-blue-100 border-blue-100'>
+      <div
+        style={{ width: width + '%' }}
+        className='transition-all h-full rounded-r-md px-1 bg-blue-500 text-[10px] text-white'
+      >
+        {width.toFixed()}%
+      </div>
+    </div>
+  );
+}
