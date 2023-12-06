c7a491abb1a3d4d1b56336e33a28302a77637020
KunHee Lim <rg4093@naver.com>
2023-12-06 23:07:44 +0900

    feat: palyground data 추가

--- src/constant/playground.ts
+++ src/constant/playground.ts
@@ -1,8 +1,15 @@
 export const playgrounds = [
   {
+    title: 'Movie Search App',
+    description: '2023.12, OMDb를 활용한 영화 검색 사이트 개발',
+    targetLink: 'https://kunhee-movie.vercel.app',
+    coverImage: '/assets/movie-search.png',
+    team: '개인',
+  },
+  {
     title: 'Kunhee Blog',
     description: '2023.11 ~ 진행, Next에 SSG를 활용한 정적 블로그 개발',
-    targetLink: 'https://kunhee-blog.vercel.app',
+    targetLink: 'https://kunheelim.com',
     coverImage: '/assets/kunhee-blog.png',
     team: '개인',
   },
